ee2d65a56e431cdabda72861df51544bca36cf02
Harry <poripori1030@gmail.com>
2019-03-18 00:51:28 +0800

    Setting Up a RecyclerView

--- app/src/main/java/my/appstudio/notes/NotesListActivity.java
+++ app/src/main/java/my/appstudio/notes/NotesListActivity.java
@@ -2,22 +2,52 @@ package my.appstudio.notes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
+import java.util.ArrayList;
+
+import my.appstudio.notes.adapters.NotesRecyclerAdapter;
 import my.appstudio.notes.models.Note;
 
 public class NotesListActivity extends AppCompatActivity {
 
     private static final String TAG = "NotesListActivity";
-    
+
+    //UI Components
+    private RecyclerView mRecyclerView;
+
+    //Vars
+    private ArrayList<Note> mNotes = new ArrayList<>();
+    private NotesRecyclerAdapter mNoteRecyclerAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes_list);
+        mRecyclerView = findViewById(R.id.recycleView);
 
-        Note note = new Note("some title", "content", "timestamp");
+        initRecyclerView();
+        insertFakeNotes();
+    }
 
-        Log.d(TAG, "onCreate: " + note.toString());
+    private void insertFakeNotes() {
+        for (int i = 0; i < 1000; i++) {
+            Note note = new Note();
+            note.setTitle("title:" + i);
+            note.setContent("content:" + i);
+            note.setTimestamp("Jan 2019");
+            mNotes.add(note);
+        }
+        mNoteRecyclerAdapter.notifyDataSetChanged();
+    }
 
+    private void initRecyclerView() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+
+        mNoteRecyclerAdapter = new NotesRecyclerAdapter(mNotes);
+        mRecyclerView.setAdapter(mNoteRecyclerAdapter);
     }
 }
