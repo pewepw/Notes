df2b750927beeabbe46e0a51fb389f3a968d5b28
Harry <poripori1030@gmail.com>
2019-03-17 15:49:37 +0800

    Defining a Data Model

--- app/src/main/java/my/appstudio/notes/NotesListActivity.java
+++ app/src/main/java/my/appstudio/notes/NotesListActivity.java
@@ -2,12 +2,22 @@ package my.appstudio.notes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import my.appstudio.notes.models.Note;
 
 public class NotesListActivity extends AppCompatActivity {
 
+    private static final String TAG = "NotesListActivity";
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes_list);
+
+        Note note = new Note("some title", "content", "timestamp");
+
+        Log.d(TAG, "onCreate: " + note.toString());
+
     }
 }
